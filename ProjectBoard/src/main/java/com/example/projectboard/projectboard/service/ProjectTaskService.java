722524a0fd163e4fbfabf248c465253f77de1258
Muhammad Hammad Ejaz <m.hammadejaz@yahoo.com>
2020-10-01 00:49:56 +0500

    my project board app

---
+++ ProjectBoard/src/main/java/com/example/projectboard/projectboard/service/ProjectTaskService.java
@@ -0,0 +1,38 @@
+package com.example.projectboard.projectboard.service;
+
+import com.example.projectboard.projectboard.domain.ProjectTask;
+import com.example.projectboard.projectboard.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask)
+    {
+        if (projectTask.getStatus() == null || projectTask.getStatus().equals(""))
+        {
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
+
+    public Iterable<ProjectTask> findAll()
+    {
+        return projectTaskRepository.findAll();
+    }
+
+    public ProjectTask findById(Long id)
+    {
+        return projectTaskRepository.getById(id);
+    }
+
+    public void delete(Long id)
+    {
+        ProjectTask projectTask = findById(id);
+        projectTaskRepository.delete(projectTask);
+    }
+}
